e9dcfdef45d81a3c13c39aa71d8939e470e55ca7
yang sun <yangsun@yangs-MacBook-Pro.local>
2022-02-23 13:03:09 -0800

    finished churn_library.py

--- churn_library.py
+++ churn_library.py
@@ -3,7 +3,6 @@
 
 # import libraries
 from pyrsistent import optional
-import shap
 import joblib
 import pandas as pd
 import numpy as np
@@ -72,7 +71,7 @@ def perform_eda(df):
 
 
 
-def encoder_helper(df, category_lst, response:optional):
+def encoder_helper(df, category_lst):
     '''
     helper function to turn each categorical column into a new column with
     propotion of churn for each category - associated with cell 15 from the notebook
@@ -80,16 +79,16 @@ def encoder_helper(df, category_lst, response:optional):
     input:
             df: pandas dataframe
             category_lst: list of columns that contain categorical features
-            response: string of response name [optional argument that could be used for naming variables or index y column]
 
     output:
-            df: pandas dataframe with new columns for
+            X: pandas dataframe with new columns for
+            y: target variables (binary)
     '''
     y = df['Churn'] 
     X = pd.DataFrame()
     
     # create target encoding for all categorical variables
-    category_lst=['Gender','Education_Level','Marital_Status','Income_Category','Card_Category']
+    # category_lst=['Gender','Education_Level','Marital_Status','Income_Category','Card_Category']
     for col in category_lst:
         temp_list=[]
         groups=df.groupby(col).mean()['Churn']
@@ -109,12 +108,11 @@ def encoder_helper(df, category_lst, response:optional):
     X[keep_cols] = df[keep_cols]
     return X,y
 
-def perform_feature_engineering(X,y, response:optional):
+def perform_feature_engineering(X,y):
     '''
     input:
               X: pandas dataframe of all X variables (should be all numerical variables)
               y: pandas series includes target variables (should be numerical binary variable)
-              response: string of response name [optional argument that could be used for naming variables or index y column]
 
     output:
               X_train: X training data
@@ -126,14 +124,14 @@ def perform_feature_engineering(X,y, response:optional):
 
     return X_train, X_test, y_train, y_test
 
-def classification_report_image(y_train,
+def classification_report_file(y_train,
                                 y_test,
                                 y_train_preds_lr,
                                 y_train_preds_rf,
                                 y_test_preds_lr,
                                 y_test_preds_rf):
     '''
-    produces classification report for training and testing results and stores report as image
+    produces classification report for training and testing results and stores report as txt file
     in images folder
     input:
             y_train: training response values
@@ -146,7 +144,23 @@ def classification_report_image(y_train,
     output:
              None
     '''
-    pass
+    plt.rc('figure', figsize=(8, 6))
+    #plt.text(0.01, 0.05, str(model.summary()), {'fontsize': 12}) old approach
+    plt.text(0.01, 1.25, str('Random Forest Train'), {'fontsize': 14}, fontproperties = 'monospace')
+    plt.text(0.01, 0.05, str(classification_report(y_test, y_test_preds_rf)), {'fontsize': 14}, fontproperties = 'monospace') # approach improved by OP -> monospace!
+    plt.text(0.01, 0.6, str('Random Forest Test'), {'fontsize': 14}, fontproperties = 'monospace')
+    plt.text(0.01, 0.7, str(classification_report(y_train, y_train_preds_rf)), {'fontsize': 14}, fontproperties = 'monospace') # approach improved by OP -> monospace!
+    plt.axis('off')
+    plt.savefig('./images/RandomForest_train_test_results.png')
+
+    plt.rc('figure', figsize=(8, 6))
+    plt.text(0.01, 1.25, str('Logistic Regression Train'), {'fontsize': 14}, fontproperties = 'monospace')
+    plt.text(0.01, 0.05, str(classification_report(y_train, y_train_preds_lr)), {'fontsize': 14}, fontproperties = 'monospace') # approach improved by OP -> monospace!
+    plt.text(0.01, 0.6, str('Logistic Regression Test'), {'fontsize': 14}, fontproperties = 'monospace')
+    plt.text(0.01, 0.7, str(classification_report(y_test, y_test_preds_lr)), {'fontsize': 14}, fontproperties = 'monospace') # approach improved by OP -> monospace!
+    plt.axis('off')
+    plt.savefig('./images/Logistic_regression_train_test_results.png')
+    
 
 
 def feature_importance_plot(model, X_data, output_pth):
@@ -160,22 +174,101 @@ def feature_importance_plot(model, X_data, output_pth):
     output:
              None
     '''
-    pass
+    # Calculate feature importances
+    importances = model.feature_importances_
+    # Sort feature importances in descending order
+    indices = np.argsort(importances)[::-1]
+
+    # Rearrange feature names so they match the sorted feature importances
+    names = [X_data.columns[i] for i in indices]
+
+    # Create plot
+    plt.figure(figsize=(20,5))
+
+    # Create plot title
+    plt.title("Feature Importance")
+    plt.ylabel('Importance')
+
+    # Add bars
+    plt.bar(range(X_data.shape[1]), importances[indices])
+
+    # Add feature names as x-axis labels
+    plt.xticks(range(X_data.shape[1]), names, rotation=90)
+    
+    # save plot
+    plt.savefig(output_pth)
 
 def train_models(X_train, X_test, y_train, y_test):
     '''
-    train, store model results: images + scores, and store models
+    train models (logistic regression and random forest with grid search).
+    store model scores to a txt file.
+    store two model. 
     input:
               X_train: X training data
               X_test: X testing data
               y_train: y training data
               y_test: y testing data
     output:
-              None
-    '''
-    pass
+              y_test_preds_rf: randomforest y prediction with testing data.
+              y_train_preds_rf: randomforest y prediction with training data.
+              y_test_preds_lr: logistic regression y prediction with testing data.
+              y_train_preds_lr: logistic regression y prediction with training data.
 
+
+    '''
+    # define moddel (random forest and logistic regression)
+    rfc = RandomForestClassifier(random_state=42)
+    lrc = LogisticRegression(max_iter=300)
+    
+    # grid search parameter
+    param_grid = { 
+        'n_estimators': [200, 500],
+        'max_features': ['auto', 'sqrt'],
+        'max_depth' : [4,5,100],
+        'criterion' :['gini', 'entropy']
+        }
+    # perform grid search on random fores
+    cv_rfc = GridSearchCV(estimator=rfc, param_grid=param_grid, cv=5)
+    cv_rfc.fit(X_train, y_train)   
+
+    # fit logistic regression
+    lrc.fit(X_train, y_train)
+    
+    # results - random forest
+    y_train_preds_rf = cv_rfc.best_estimator_.predict(X_train)
+    y_test_preds_rf = cv_rfc.best_estimator_.predict(X_test)
+    # results - logistic regression
+    y_train_preds_lr = lrc.predict(X_train)
+    y_test_preds_lr = lrc.predict(X_test)   
+    
+    # store two models
+    joblib.dump(cv_rfc.best_estimator_, './models/rfc_model.pkl')
+    joblib.dump(lrc, './models/logistic_model.pkl')
+
+    # store model results
+    classification_report_file(y_train,
+                                y_test,
+                                y_train_preds_lr,
+                                y_train_preds_rf,
+                                y_test_preds_lr,
+                                y_test_preds_rf)
+
+    return y_test_preds_rf,y_train_preds_rf,y_test_preds_lr,y_train_preds_lr
 
 if __name__=='__main__':
+     # read in data
      df=import_data('./data/bank_data.csv')
+     # eda analysis
      perform_eda(df)
+     # list of categorical variables
+     category_lst=['Gender','Education_Level','Marital_Status','Income_Category','Card_Category']
+     # target encoding categorical variables
+     X,y=encoder_helper(df,category_lst)
+     # separate train and test data
+     X_train, X_test, y_train, y_test = perform_feature_engineering(X,y)
+     # train the model and outoput predictions results
+     y_test_preds_rf,y_train_preds_rf,y_test_preds_lr,y_train_preds_lr=train_models( X_train, X_test, y_train, y_test)
+     # load back the best model (random forest)
+     rfc_model = joblib.load('./models/rfc_model.pkl')
+     # create feature importance plot
+     feature_importance_plot(rfc_model,X,'./images/feature_importance_rf.png')
